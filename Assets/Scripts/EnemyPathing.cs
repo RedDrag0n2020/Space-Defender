b793dba11466239ada4f7cfe7f993207363c651e
reddragonstudios <kdmcoder2000@gmail.com>
2019-07-29 16:45:32 -0400

    Added waypoints and path(0) game objects. Added the Enemy Pathing.cs script. Created the Path0Wapoints List. Connected waypoint objects to enemy prefab. 7/29/19

---
+++ Assets/Scripts/EnemyPathing.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemyPathing : MonoBehaviour {
+
+	[SerializeField] List<Transform> Path0Waypoints;
+
+
+    // Use this for initialization
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
