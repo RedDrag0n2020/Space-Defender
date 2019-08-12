2f99f5374e0fa3760d62fcd55bed1ac2315d081a
reddragonstudios <kdmcoder2000@gmail.com>
2019-08-12 16:39:02 -0400

    Added pathfinding using waypoints feature 8/12/19.

--- Assets/Scripts/EnemyPathing.cs
+++ Assets/Scripts/EnemyPathing.cs
@@ -5,15 +5,49 @@ using UnityEngine;
 public class EnemyPathing : MonoBehaviour {
 
 	[SerializeField] List<Transform> Path0Waypoints;
+    [SerializeField] float moveSpeed = 2f;
+    int waypointIndex = 0;
+
 
 
     // Use this for initialization
     void Start () {
-		
+
+        transform.position = Path0Waypoints[waypointIndex].transform.position;
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        Move();
+
+    }
+
+    private void Move()
+    {
+        if (waypointIndex <= Path0Waypoints.Count - 1)
+
+        {
+            var targetPosition = Path0Waypoints[waypointIndex].transform.position;
+            var movementThisFrame = moveSpeed * Time.deltaTime;
+
+            transform.position = Vector2.MoveTowards(transform.position, targetPosition, movementThisFrame);
+
+            if (transform.position == targetPosition)
+
+            {
+                waypointIndex++;
+
+            }
+
+
+        }
+
+        else
+        {
+            Destroy(gameObject);
+
+        }
+    }
 }
