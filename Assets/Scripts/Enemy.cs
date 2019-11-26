1de595c7467d631f0368dcfdaafde4f2dadfce1b
reddragonstudios <kdmcoder2000@gmail.com>
2019-11-26 15:44:55 -0500

    Corrected issue where enemy ships in wave did not shoot. Solution: I forgot to pick a sprite for the Enemy Laser in the Prefab. 11-26-19

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -1,21 +1,59 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class Enemy : MonoBehaviour {
 
     [SerializeField] float health = 100f;
+    [SerializeField] float shotCounter;
+    [SerializeField] float minTimeBetweenShots = 0.2f;
+    [SerializeField] float maxTimeBetweenShots = 3f;
+    [SerializeField] GameObject enemyLaserPrefab;
+    [SerializeField] float projectileSpeed = 10f;
+    
 
-	// Use this for initialization
-	void Start () {
-		
+
+    // Use this for initialization
+    void Start ()
+    {
+        shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+
 	}
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update () 
+    {
+        CountDownandShoot();
 
+    }
+
+    private void CountDownandShoot()
+    {
+        shotCounter -= Time.deltaTime;
+
+        if (shotCounter <= 0f)
+
+        {
+            Fire();
+
+            shotCounter = UnityEngine.Random.Range(minTimeBetweenShots, maxTimeBetweenShots);
+        }
+    }
+
+    private void Fire()
+
+    {
+        GameObject enemyLaser = Instantiate(enemyLaserPrefab, transform.position, Quaternion.identity) as GameObject;
+
+        enemyLaser.GetComponent<Rigidbody2D>().velocity = new Vector2(0, -projectileSpeed);
+        
+
+        
+    }
+
+   
+   
     private void OnTriggerEnter2D(Collider2D other)
     {
         DamageDealer damageDealer = other.gameObject.GetComponent<DamageDealer>();
