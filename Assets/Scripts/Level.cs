e023a514609323b224c55555deddedc48304096a
reddragonstudios <kdmcoder2000@gmail.com>
2020-02-07 18:36:49 -0500

    Added Start and Game Over scenes. Added Text Mesh Pro and new font. Added scene manager and Level.cs file. 2-07-2020

---
+++ Assets/Scripts/Level.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class Level : MonoBehaviour {
+
+	public void LoadGameOver()
+
+	{
+		SceneManager.LoadScene("Game Over");
+
+
+	}
+
+	public void LoadGame()
+
+	{
+
+		SceneManager.LoadScene("Game");
+
+	}
+
+	public void LoadStartMenu()
+
+	{
+
+		SceneManager.LoadScene(0);
+
+	}
+
+	public void QuitGame()
+
+	{
+
+		Application.Quit();
+
+	}
+}
