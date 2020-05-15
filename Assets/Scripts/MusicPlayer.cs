3b828f561c4427d9b9c6726bdb7fb594a4550f8e
reddragonstudios <kdmcoder2000@gmail.com>
2020-05-15 13:47:16 -0400

    Added music asset, Music Player object., and MusicPlayer.cs script. Updated design document. 5-15-2020

---
+++ Assets/Scripts/MusicPlayer.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicPlayer : MonoBehaviour {
+
+	// Use this for initialization
+	void Awake () 
+	
+	{
+		SetUpSingleton();
+		
+
+	}
+
+	private void SetUpSingleton()
+
+	{
+		// GetType method gets the type of this class i.e., Music Player
+				if(FindObjectsOfType(GetType()).Length > 1)
+
+					{
+						Destroy(gameObject);
+
+					}
+				else
+
+					{
+						DontDestroyOnLoad(gameObject);
+
+					}
+
+
+	}
+
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
